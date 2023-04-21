a036b6e556b98912b4c1ed9841ea9c4beb30d8a4
nima karimian <niimak97@gmail.com>
2023-04-21 14:41:37 +0330

    init

---
+++ src/main/java/com/illutech/advertsite/AdvertSiteApplication.java
@@ -0,0 +1,13 @@
+package com.illutech.advertsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvertSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvertSiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/illutech/advertsite/AdvertSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.illutech.advertsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvertSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
