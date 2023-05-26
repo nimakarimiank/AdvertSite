a32c0ad2418722559612325292f8ed1b680a2b01
nima karimian <niimak97@gmail.com>
2023-05-26 15:46:49 +0330

    Setting up the JWT

--- src/main/java/com/illutech/advertsite/config/jwtconfig/JwtService.java
+++ src/main/java/com/illutech/advertsite/config/jwtconfig/JwtService.java
@@ -1,12 +1,32 @@
 package com.illutech.advertsite.config.jwtconfig;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
+    private static final String SECRET_KEY="26452948404D635166546A576E5A7234753778217A25432A462D4A614E645267";
 
     public String extractUserName(String jwtToken) {
         //TODO extractUserName
         return null;
+    }
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBits = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBits);
     }
 }
